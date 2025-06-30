915d940e59b96d42a66bd4b1a377cad30e647f08
matheusmendespascoal2025 <matheus.pascoal@escola.pr.gov.br>
2025-06-30 18:42:03 -0300

    Ultima alteração

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ let elemento = document.querySelector("#nome-usuario");
 elemento.textContent = nomeUsuario;
 }
 inserirNome();
+
+let linguagens = ["Javascript", "Python", "C"];
+console.log(linguagens[0]);
+console.log(linguagens[1]);
+console.log(linguagens[2]);
+
