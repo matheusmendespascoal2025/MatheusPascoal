30a6830bce21dea32d5c0e05e8fc6d5265dd133e
matheusmendespascoal2025 <matheus.pascoal@escola.pr.gov.br>
2025-07-01 11:56:42 -0300

    Ultima Atualização

--- script.js
+++ script.js
@@ -10,4 +10,4 @@ console.log(linguagens[0]);
 console.log(linguagens[1]);
 console.log(linguagens[2]);
 const item = document.querySelector("#lista");
-item.textContent = linguagens[0];
+item.textContent = linguagens[2];
