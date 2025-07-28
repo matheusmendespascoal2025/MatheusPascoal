fd2b9992cd50e305fef86df8ec788c8a57118894
matheusmendespascoal2025 <matheus.pascoal@escola.pr.gov.br>
2025-07-28 18:40:47 -0300

    salvo

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ console.log(linguagens[1]);
 console.log(linguagens[2]);
 const item = document.querySelector("#lista");
 item.textContent = linguagens[0];
+
+let aluno1 = {
+  nome: "Gabriel",
+  idade: 13,
+  anoLetivo: "2ºsérie",
+  materiasFavoritas: ["Geografia","Programação","Arte"]
+}
